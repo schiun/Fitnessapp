c927aede571de1bb8641c822d2c18044c198e8ff
schiun <schiun.tan@gmail.com>
2025-07-18 23:44:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-(cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF'
-diff --git a/FitDaily-Complete-Package/script.js b/FitDaily-Complete-Package/script.js
---- a/FitDaily-Complete-Package/script.js
-+++ b/FitDaily-Complete-Package/script.js
-@@ -0,0 +1,582 @@
 +// Workout Data
 +const workoutData = {
 +    push: {
